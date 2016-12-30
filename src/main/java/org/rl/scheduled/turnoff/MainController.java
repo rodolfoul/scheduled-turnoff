c8429f992969e77b01ca83aaf00c22221b099d24
Rodolfo Labsch <w.rodolfo.labsch@gmail.com>
2016-12-30 17:14:08 -0200

    Power control job functional.

--- src/main/java/org/rl/scheduled/turnoff/MainController.java
+++ src/main/java/org/rl/scheduled/turnoff/MainController.java
@@ -1,26 +1,47 @@
 package org.rl.scheduled.turnoff;
 
-import org.quartz.CronScheduleBuilder;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
-import org.quartz.impl.StdSchedulerFactory;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class MainController {
 	private static final String DESLIGAMENTO = "0 30 1 ? * *";
 
 
-	public static void main(String[] args) throws SchedulerException {
-		JobDetail job = JobBuilder.newJob(DBusTurnoff.class).build();
-		Trigger trigger = TriggerBuilder.newTrigger().withSchedule(CronScheduleBuilder.cronSchedule(DESLIGAMENTO))
-		                                .build();
+	public static void main(String[] args) throws IOException, InterruptedException {
+		restartWithPermissions();
 
-		//schedule it
-		Scheduler scheduler = new StdSchedulerFactory().getScheduler();
-		scheduler.start();
-		scheduler.scheduleJob(job, trigger);
+//		JobDetail job = JobBuilder.newJob(PowerControlJob.class).build();
+//		Trigger trigger = TriggerBuilder.newTrigger().withSchedule(CronScheduleBuilder.cronSchedule(DESLIGAMENTO))
+//		                                .build();
+//
+//		//schedule it
+//		Scheduler scheduler = new StdSchedulerFactory().getScheduler();
+//		scheduler.start();
+//		scheduler.scheduleJob(job, trigger);
+	}
+
+	private static void restartWithPermissions() throws IOException, InterruptedException {
+		String userName = System.getProperty("user.name");
+		System.out.println("Current User: " + userName);
+
+		if (!"root".equals(userName)) {
+			String jvmPath = new File(System.getProperty("java.home"), "bin/java").getAbsolutePath();
+			URL[] urls = ((URLClassLoader) ClassLoader.getSystemClassLoader()).getURLs();
+			String classPath = Arrays.stream(urls).map(URL::getFile).collect(Collectors.joining(":"));
+			String mainClassName = System.getProperty("sun.java.command");
+			ProcessBuilder gksu = new ProcessBuilder("gksu",
+			                                         String.format("%s -cp %s %s", jvmPath, classPath, mainClassName));
+			gksu.inheritIO();
+			Process process = gksu.start();
+			int status = process.waitFor();
+			if (status != 0) {
+				System.err.println("Error, process exited with status code " + status);
+			}
+			System.exit(status);
+		}
 	}
 }
--- src/main/java/org/rl/scheduled/turnoff/PowerControlJob.java
+++ src/main/java/org/rl/scheduled/turnoff/PowerControlJob.java
@@ -7,9 +7,16 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Field;
+import java.time.Duration;
+import java.time.Instant;
 
-public class DBusTurnoff implements Job {
+public class PowerControlJob implements Job {
+
+	private final Duration POWER_OFF_DURATION = Duration.ofHours(4);
 
 	static {
 		try {
@@ -23,11 +30,28 @@ public class DBusTurnoff implements Job {
 			e.printStackTrace();
 			System.exit(1);
 		}
-
 	}
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
+		try {
+			setStartUpTime();
+			powerOff();
+		} catch (DBusException | IOException e) {
+			throw new JobExecutionException(e);
+		}
+	}
+
+	private void setStartUpTime() throws IOException {
+		try (DataOutputStream wakeAlarmStream = new DataOutputStream(
+				new FileOutputStream("/sys/class/rtc/rtc0/wakealarm"))) {
+			Instant instant = Instant.now().plus(POWER_OFF_DURATION);
+			wakeAlarmStream.writeChars(String.format("0\n%s\n", instant.getEpochSecond()));
+		}
+
+	}
+
+	private void powerOff() throws JobExecutionException, DBusException {
 		DBusConnection conn = null;
 		try {
 			conn = DBusConnection.getConnection(DBusConnection.SYSTEM);
@@ -35,8 +59,6 @@ public class DBusTurnoff implements Job {
 					.getRemoteObject("org.freedesktop.login1", "/org/freedesktop/login1", Manager.class);
 
 			loginManager.PowerOff(true);
-		} catch (DBusException e) {
-			throw new JobExecutionException(e);
 		} finally {
 			if (conn != null) {
 				conn.disconnect();
