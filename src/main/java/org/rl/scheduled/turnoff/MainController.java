62afc4be8941cf19a0400a079778336318fc4be7
Rodolfo Labsch <w.rodolfo.labsch@gmail.com>
2017-01-03 15:14:22 -0200

    Adding properties control for cron expressions.

--- src/main/java/org/rl/scheduled/turnoff/MainController.java
+++ src/main/java/org/rl/scheduled/turnoff/MainController.java
@@ -6,21 +6,23 @@ import com.sun.jna.Native;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.quartz.CronExpression;
-import org.quartz.CronScheduleBuilder;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
+import org.quartz.CronTrigger;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
-import org.quartz.Trigger;
-import org.quartz.TriggerBuilder;
-import org.quartz.impl.StdSchedulerFactory;
 
-import java.io.File;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 import java.text.ParseException;
 import java.time.Instant;
 import java.time.ZoneId;
@@ -28,8 +30,12 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Properties;
 
 public class MainController {
+	private CronTrigger trigger;
+	private Scheduler scheduler;
+
 	interface CLibrary extends Library {
 		CLibrary INSTANCE = (CLibrary) Native.loadLibrary("c", CLibrary.class);
 
@@ -39,41 +45,97 @@ public class MainController {
 	}
 
 	static {
-		File file = new File(MainController.class.getProtectionDomain().getCodeSource().getLocation().getPath());
-		CLibrary.INSTANCE.chdir(file.getParentFile().toString());
-		System.setProperty("user.dir", file.toString());
+		Path jarPath = Paths.get(MainController.class.getResource("/").getPath());
+		CLibrary.INSTANCE.chdir(jarPath.getParent().toString());
+		System.setProperty("user.dir", jarPath.toString());
 	}
-
-	public static final String SHUTDOWN_CRON = "0 30 1 ? * *";
-	public static final String STARTUP_CRON = "0 0 6 ? * *";
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
-	public static void main(String[] args) throws IOException, SchedulerException, ParseException {
-		checkRootPermission();
+	private Properties properties;
 
-		JobDetail job = JobBuilder.newJob(PowerControlJob.class).build();
-		CronExpression shutdownCron = new CronExpression(SHUTDOWN_CRON);
-		LOGGER.info("Next shutdown to occur at {}", shutdownCron.getNextValidTimeAfter(new Date()).toInstant().atZone(
-				ZoneId.systemDefault()));
-		Trigger trigger = TriggerBuilder.newTrigger().withSchedule(CronScheduleBuilder.cronSchedule(shutdownCron))
-		                                .build();
-
-		Scheduler scheduler = new StdSchedulerFactory().getScheduler();
-		scheduler.start();
-		scheduler.scheduleJob(job, trigger);
-
-		schedulePowerOn();
+	public static void main(String[] args) {
+		new MainController().start();
 	}
 
-	private static void schedulePowerOn() throws IOException {
+
+	public void start() {
+		checkRootPermission();
+
+		configureProperties();
+		configureScheduling();
+
+	}
+
+	private void configureProperties() {
+
+		properties = new Properties();
+		Path propertiesPath = Paths.get(MainController.class.getResource("/cron-times.properties").getFile());
+		loadProperties(propertiesPath);
+
+		new Thread(() -> {
+			Path watchedDir = propertiesPath.getParent();
+			try (WatchService watchService = FileSystems.getDefault().newWatchService()) {
+				WatchKey registerKey = watchedDir.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+				while (true) {
+					try {
+						WatchKey eventKey = watchService.take();
+						for (WatchEvent<?> watchEvent : eventKey.pollEvents()) {
+							Path path = (Path) watchEvent.context();
+							if (propertiesPath.equals(watchedDir.resolve(path))) {
+								loadProperties(propertiesPath);
+								configureScheduling();
+							}
+						}
+					} catch (InterruptedException e) {
+						LOGGER.warn("Interrupted while watching path for changes.", e);
+					}
+					registerKey.reset();
+				}
+
+			} catch (IOException e) {
+				LOGGER.error(
+						"Exception while configuring watch service for properties file. " +
+						"Properties reloading will not function", e);
+			}
+		}).start();
+	}
+
+	private void loadProperties(Path propertiesPath) {
+		try (InputStream is = new BufferedInputStream(Files.newInputStream(propertiesPath))) {
+			properties.load(is);
+
+		} catch (IOException e) {
+			LOGGER.error("Failed loading properties file.", e);
+		}
+	}
+
+	private void configureScheduling() {
+		schedulePowerOn();
+		schedulePowerOff();
+	}
+
+
+	private void schedulePowerOff() {
 		try {
-			CronExpression turnOnExpression = new CronExpression(MainController.STARTUP_CRON);
+			QuartzController.rescheduleJob(new CronExpression(properties.getProperty("shutdown.cron")));
+
+		} catch (ParseException e) {
+			LOGGER.warn("Could not parse the given cron expression for shutdown.", e);
+		} catch (SchedulerException e) {
+			LOGGER.error("Failed while configuring cron for shutdown.", e);
+		}
+	}
+
+	private void schedulePowerOn() {
+		try {
+			CronExpression turnOnExpression = new CronExpression(properties.getProperty("startup.cron"));
 			Instant turnOnInstant = turnOnExpression.getNextValidTimeAfter(new Date()).toInstant();
 
 			long turnOnEpoch = turnOnInstant.getEpochSecond();
 			LOGGER.info("Setting next startup to {} -> {} -> {}", turnOnInstant, turnOnEpoch,
 			            turnOnInstant.atZone(ZoneId.systemDefault()));
+
 			try (Writer wakeAlarmStream = Files.newBufferedWriter(Paths.get("/sys/class/rtc/rtc0/wakealarm"))) {
 				wakeAlarmStream.write(String.format("0\n"));
 			}
@@ -82,52 +144,67 @@ public class MainController {
 			}
 
 		} catch (ParseException e) {
-			LOGGER.error("Unreachable code, as ");
+			LOGGER.warn("Could not parse cron expression for startup.cron", e);
+		} catch (IOException e) {
+			LOGGER.error("Failed while configuring next startup time.");
 		}
 	}
 
 
-	private static void checkRootPermission() throws IOException {
+	private void checkRootPermission() {
 		String userName = System.getProperty("user.name");
 		LOGGER.debug("Current user: {}", userName);
 
 		if (!"root".equals(userName)) {
-			List<String> currentCmd = Arrays.asList(new String(
-					Files.readAllBytes(Paths.get("/proc", Integer.toString(CLibrary.INSTANCE.getpid()), "cmdline")),
-					StandardCharsets.UTF_8).split("\0"));
-
-			StringBuilder cmdLine = new StringBuilder();
-			boolean cpArgument = false;
-			for (ListIterator<String> it = currentCmd.listIterator(); it.hasNext(); ) {
-				String argument = it.next();
-				if ("-cp".equals(argument) || "-classpath".equals(argument)) {
-					cmdLine.append(" ").append(argument).append(" ").append(it.next());
-					cpArgument = true;
-
-				} else if (it.previousIndex() == 0) {
-					cmdLine.append(" ").append(argument);
-
-				} else if ("-jar".equals(argument)) {
-					cmdLine.append(" ").append(argument).append(" ").append(it.next());
-				}
-			}
-			if (cpArgument) {
-				cmdLine.append(System.getProperty("sun.java.command"));
-			}
-
-			ProcessBuilder gksu = new ProcessBuilder("gksu", cmdLine.toString());
-			gksu.inheritIO();
-			Process process = gksu.start();
-			int status = 0;
 			try {
-				status = process.waitFor();
-				if (status != 0) {
-					System.err.println("Error, process exited with status code " + status);
+				String cmdLine = getFullCommandLine();
+
+				ProcessBuilder gksuBuilder = new ProcessBuilder("gksu", cmdLine);
+				gksuBuilder.inheritIO();
+				Process gksuProcess = gksuBuilder.start();
+				int status = 0;
+
+				try {
+					status = gksuProcess.waitFor();
+				} catch (InterruptedException e) {
+					LOGGER.error("Unreacheable code, since no threads interrupt the current one.", e);
 				}
-			} catch (InterruptedException e) {
-				LOGGER.error("Unreacheable code, since no threads interrupt the current one.", e);
+
+				if (status != 0) {
+					LOGGER.error("Error, process exited with status code {}", status);
+				}
+				System.exit(status);
+
+			} catch (IOException e) {
+				LOGGER.error("Failed while getting root privileges.", e);
+				System.exit(1);
 			}
-			System.exit(status);
 		}
+	}
+
+	private String getFullCommandLine() throws IOException {
+		List<String> currentCmd = Arrays.asList(new String(
+				Files.readAllBytes(Paths.get("/proc", Integer.toString(CLibrary.INSTANCE.getpid()), "cmdline")),
+				StandardCharsets.UTF_8).split("\0"));
+
+		StringBuilder cmdLine = new StringBuilder();
+		boolean cpArgument = false;
+		for (ListIterator<String> it = currentCmd.listIterator(); it.hasNext(); ) {
+			String argument = it.next();
+			if ("-cp".equals(argument) || "-classpath".equals(argument)) {
+				cmdLine.append(" ").append(argument).append(" ").append(it.next());
+				cpArgument = true;
+
+			} else if (it.previousIndex() == 0) {
+				cmdLine.append(" ").append(argument);
+
+			} else if ("-jar".equals(argument)) {
+				cmdLine.append(" ").append(argument).append(" ").append(it.next());
+			}
+		}
+		if (cpArgument) {
+			cmdLine.append(System.getProperty("sun.java.command"));
+		}
+		return cmdLine.toString();
 	}
 }
---
+++ src/main/java/org/rl/scheduled/turnoff/QuartzController.java
@@ -0,0 +1,108 @@
+package org.rl.scheduled.turnoff;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.freedesktop.dbus.DBusConnection;
+import org.freedesktop.dbus.exceptions.DBusException;
+import org.freedesktop.login1.Manager;
+import org.quartz.CronExpression;
+import org.quartz.CronScheduleBuilder;
+import org.quartz.Job;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class QuartzController {
+
+	private static final Logger LOGGER = LogManager.getLogger();
+	private static Scheduler scheduler;
+	private static Trigger currentTrigger;
+	private static JobDetail job = JobBuilder.newJob(PowerOffJob.class).build();
+
+	static {
+		try {
+			scheduler = new StdSchedulerFactory().getScheduler();
+			scheduler.start();
+		} catch (SchedulerException e) {
+			LOGGER.error("Could not set up quartz scheduler, exiting...", e);
+			System.exit(1);
+		}
+	}
+
+
+	public static void rescheduleJob(CronExpression cronExpression) throws SchedulerException {
+		LOGGER.info("Next shutdown to occur at {}",
+		            cronExpression.getNextValidTimeAfter(new Date()).toInstant().atZone(
+				            ZoneId.systemDefault()));
+
+		Trigger trigger = TriggerBuilder.newTrigger()
+		                                .withSchedule(CronScheduleBuilder.cronSchedule(cronExpression))
+		                                .forJob(job)
+		                                .build();
+		if (currentTrigger != null) {
+			scheduler.rescheduleJob(currentTrigger.getKey(), trigger);
+		} else {
+			scheduler.scheduleJob(job, trigger);
+		}
+		currentTrigger = trigger;
+	}
+
+	private static class PowerOffJob implements Job {
+
+		static {
+			try {
+				String libLookupPath = "java.library.path"; //dbus setup
+				File libDir = new File(File.class.getResource("/libunix-java.so").getPath()).getParentFile();
+				System.setProperty(libLookupPath, System.getProperty(libLookupPath) + ":" + libDir);
+				Field sys_paths = ClassLoader.class.getDeclaredField("sys_paths");
+				sys_paths.setAccessible(true);
+				sys_paths.set(ClassLoader.class, null);
+			} catch (NoSuchFieldException | IllegalAccessException e) {
+				LOGGER.error("Could not set up dbus lib. Exiting...", e);
+				System.exit(1);
+			}
+		}
+
+		@Override
+		public void execute(JobExecutionContext context) throws JobExecutionException {
+			try {
+				LOGGER.info("Starting shutdown sequence");
+				powerOffNow();
+
+			} catch (DBusException e) {
+				LOGGER.error("Exception while during power off", e);
+				throw new JobExecutionException(e);
+			}
+		}
+
+		private void powerOffNow() throws DBusException {
+			DBusConnection conn = null;
+			try {
+				conn = DBusConnection.getConnection(DBusConnection.SYSTEM);
+				Manager loginManager = conn
+						.getRemoteObject("org.freedesktop.login1", "/org/freedesktop/login1", Manager.class);
+
+				loginManager.PowerOff(true);
+
+			} finally {
+				if (conn != null) {
+					conn.disconnect();
+				}
+			}
+
+			LOGGER.debug("Power off signal successful exiting.");
+			System.exit(0);
+		}
+	}
+}
